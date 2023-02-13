b14cb9930dbb0b123df8a4e77eca21813c2774e5
Mahmoud_nfz <mahmoudhnefzi@gmail.com>
2023-02-13 17:50:25 +0100

    finished assignment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [TodoModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
